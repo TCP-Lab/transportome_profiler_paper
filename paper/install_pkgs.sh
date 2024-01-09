23b2c12b1881ae3e836ad644b4c04d72338bb60d
Luca Visentin <luca.visentin@unito.it>
2024-01-09 13:51:24 +0100

    feat: Initial commit

---
+++ paper/install_pkgs.sh
@@ -0,0 +1,21 @@
+#/usr/bin/env bash
+
+tlmgr install \
+    collection-fontsextra \
+    collection-fontsrecommended \
+    preprint \
+    fontawesome5 \
+    enumitem \
+    collectbox \
+    glossaries \
+    titlesec \
+    tocloft \
+    biblatex \
+    xcolor \
+    listings \
+    awesomebox \
+    fancyhdr \
+    abstract \
+    mwe \
+    adjustbox \
+    biblatex-nature
